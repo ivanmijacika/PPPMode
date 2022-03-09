dc01b7f6bf2ae79fc64640385242c2e89c90b7aa
JustinMorrill <jmorrill20@stuy.edu>
2022-03-09 09:26:07 -0500

    Linked html with __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,29 @@ app = Flask(__name__)    #create Flask object
 app.secret_key = os.urandom(32) #create random key
   
 @app.route('/')
+@app.route('/home')
 def home():
-    return render_template("home.html", user = "Robert")
+    return render_template("home.html")
     
+@app.route('/play')
+def play():
+    return render_template("play.html")
+    
+@app.route('/login')
+def login():
+    return render_template("login.html")
+    
+@app.route('/logout')
+def logout():
+    return render_template("logout.html")
+    
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template("leaderboard.html")
+    
+@app.route('/settings')
+def settings():
+    return render_template("settings.html")
   
 if __name__ == "__main__":
     app.debug = True
