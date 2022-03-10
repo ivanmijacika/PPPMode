4152c80e5f8596fea46516b23330dcdf35737f0b
naronesty <noakai@gmail.com>
2022-03-10 10:38:42 -0500

    canvas

--- app/static/js/snake.js
+++ app/static/js/snake.js
@@ -7,11 +7,17 @@ var c = document.getElementById('slate');
 var ctx = c.getContext("2d");
 
 ctx.fillStyle = "#37C543";
-ctx.fillRect(0, 0, 1200, 600); 
+ctx.fillRect(0, 0, 1200, 600);
 
-for (let i = 100; i < 1200; i += 100) {
-    for (let j = 100; j < 600; j += 100) {
+for (let i = 0; i <= 1200; i += 100) {
+    for (let j = 0; j <= 600; j += 100) {
         ctx.fillStyle = "#199B24";
-        ctx.fillRect(i, j, i+100, j+100); 
+        ctx.fillRect(i, j, 50, 50);
+    }
+}
+for (let i = 50; i <= 1200; i += 100) {
+    for (let j = 50; j <= 600; j += 100) {
+        ctx.fillStyle = "#199B24";
+        ctx.fillRect(i, j, 50, 50);
     }
 }
