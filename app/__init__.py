66708f3bd8fd9d0c1ff49d546a500f3e97210ba2
naronesty <noakai@gmail.com>
2022-03-09 22:38:41 -0500

    authentication system

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template, redirect, url_for, request, session
-import os
+# PPP Mode: Ivan Mijacika, Qina Liu, Justin Morrill, Noakai Aronesty
+# SoftDev pd2
+# P02 -- Snake++
+
+from os import urandom
+from flask import Flask, render_template, request, session, redirect, url_for
+from auth import *
+from db import *
+import sqlite3
 
 app = Flask(__name__)    #create Flask object
-app.secret_key = os.urandom(32) #create random key
+app.secret_key = urandom(32) #create random key
   
 @app.route('/')
 @app.route('/home')
 def home():
+    ''' Loads the landing page '''
     return render_template("home.html")
     
 @app.route('/play')
@@ -15,11 +23,13 @@ def play():
     
 @app.route('/login')
 def login():
+    ''' Displays login page '''
     return render_template("login.html")
-    
-@app.route('/logout')
-def logout():
-    return render_template("logout.html")
+
+@app.route("/register")
+def register():
+    ''' Displays register page '''
+    return render_template('register.html')
     
 @app.route('/leaderboard')
 def leaderboard():
@@ -29,6 +39,78 @@ def leaderboard():
 def settings():
     return render_template("settings.html")
   
+@app.route("/logout")
+def logout():
+    ''' Logout user by deleting user from session dict. Redirects to loginpage '''
+    # Delete user. This try... except... block prevent an error from ocurring when the logout page is accessed from the login page
+    try:
+        session.pop('username')
+    except KeyError:
+        return redirect(url_for('home'))
+    # Redirect to login page
+    return redirect(url_for('home'))
+
+# authetication of login
+@app.route("/auth", methods=['GET', 'POST'])
+def authenticate():
+    ''' Checks whether method is get, post. If get method, then redirect to
+       loginpage. If post, then authenticate the username and password, rendering
+       the error page if incorrect and the response.html if correct username/pass. '''
+
+    # Variables
+    method = request.method
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # Get vs Post
+    if method == 'GET':
+        return redirect(url_for('home'))
+
+    auth_state = auth_user(username, password)
+    if auth_state == True:
+        session['username'] = username
+        return redirect(url_for('home'))
+    elif auth_state == "bad_pass":
+        return render_template('login.html', input="bad_pass")
+    elif auth_state == "bad_user":
+        return render_template('login.html', input="bad_user")
+
+@app.route("/rAuth", methods=['GET', 'POST'])
+def rAuthenticate():
+    ''' Authentication of username and passwords given in register page from user '''
+
+    method = request.method
+    username = request.form.get('username')
+    password0 = request.form.get('password0')
+    password1 = request.form.get('password1')
+
+    if method == 'GET':
+        return redirect(url_for('register'))
+
+    if method == 'POST':
+        # error when no username is inputted
+        if len(username) == 0:
+            return render_template('register.html', given="username")
+        # error when no password is inputted
+        elif len(password0) == 0:
+            return render_template('register.html', given="password")
+        elif len(password0) < 8:
+            return render_template('register.html', given="password greater than 8 characters")
+        # a username and password is inputted
+        # a username and password is inputted
+        else:
+            # if the 2 passwords given don't match, will display error saying so
+            if password0 != password1:
+                return render_template('register.html', mismatch=True)
+            else:
+                # creates user account b/c no fails
+                if create_user(username, password0):
+                    return render_template('login.html', input='success')
+                # does not create account because create_user failed (username is taken)
+                else:
+                    return render_template('register.html', taken=True)
+
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
---
+++ app/db.py
@@ -0,0 +1,3 @@
+# PPP Mode: Ivan Mijacika, Qina Liu, Justin Morrill, Noakai Aronesty
+# SoftDev pd2
+# P02 -- Snake++
