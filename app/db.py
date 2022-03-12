1d9f94cd72a6469894e3078ab26d2e91fd9e0303
QinaL <qina614liu@gmail.com>
2022-03-12 18:51:43 -0500

    db scores

--- app/db.py
+++ app/db.py
@@ -17,25 +17,52 @@ def add_score(username, score, mode):
     users = []
     for a_tuple in c.fetchall():
         users.append(a_tuple[0])
-    
+
     if username in users:
-        c.execute("SELECT (?) FROM scores WHERE usernames = username;", (highscore_name))
+        dbScore = get_score(username, mode)
+        if (dbScore is None) or (score > dbScore):
+            # no prev score in this mode or new highscore achieved
+            c.execute("UPDATE scores SET {mode} = ? WHERE usernames = ?".format(mode=highscore_name), (score, username))
+    else:
+        # username does not have row yet
+        c.execute("INSERT INTO scores (usernames, {mode}) VALUES (?, ?)".format(mode=highscore_name), (username, score,))
+    '''
+    TIP : SQL parameters can only apply to insert data, not table names; hence .format()
+    '''
+    db.commit()
+    db.close()
+    '''
+    if username in users:
+        c.execute("SELECT (?) FROM scores WHERE usernames = (?);", (highscore_name, username))
         if score > c.fetchone():
             c.execute("UPDATE scores SET (?) = (?) WHERE usernames = (?);", (highscore_name, score, username))
         db.commit()
     else:
     	c.execute("INSERT INTO scores VALUES (?, ?, ?, ?, ?, ?, ?, ?);", (username, 0, 0, 0, 0, 0, 0, 0))
     	#c.execute("UPDATE scores SET (?) = (?) WHERE usernames = (?);", (highscore_name, score, username))
-    	c.execute("UPDATE scores SET highscore_basic = 7652 WHERE usernames = Robert;")
-    	
+    	#c.execute("UPDATE scores SET highscore_basic = 7652 WHERE usernames = Robert;")
+    '''
+
 def get_score(username, mode):
     '''Gets the players highscore for a certain mode'''
     
     db = sqlite3.connect(DB_file)
     c = db.cursor()
+    highscore_name = "highscore_" + mode
     
-    c.execute("SELECT (?) FROM scores WHERE usernames = username", (highscore_name))
-    return c.fetchone()
+    c.execute("SELECT {mode} FROM scores WHERE usernames = (?)".format(mode = highscore_name), (username,))
+    return c.fetchone()[0]
     	
 def top_n(n):
     '''Gets the top n scores'''
+
+# for testing & debugging purposes
+def get_db():
+    db = sqlite3.connect(DB_file)
+    c = db.cursor()
+
+    c.execute("SELECT * from scores")
+    print(c.fetchall())
+
+    db.close()
+
