db056cd043831ab3d13f1815bf27136173ec3e78
QinaL <qina614liu@gmail.com>
2022-03-17 21:12:56 -0400

    leaderboard data

--- app/static/js/leaderboard.js
+++ app/static/js/leaderboard.js
@@ -2,9 +2,12 @@
 // SoftDev pd2
 // P02 -- Snake++
 
-let items = document.getElementsByTagName("td");
 
+// users, scores in the leaderboard table
+let items = document.getElementsByTagName("td");
 let update_data = (users, scores) => {
+    // items goes through what's in the table (1 user, 1 score per row)
+    // row goes through the data arrays 
     let row = 0;
     for (let i = 0; i < items.length; i += 2){
         items[i].innerHTML = users[row];
@@ -13,18 +16,20 @@ let update_data = (users, scores) => {
     }
 }
 
-
-
+// comms with python to update 
 let py_data = (mode) => {
     fetch('/leaderboard_data', {
+        // sends selected mode to python
         headers: { 'Content-Type': 'application/json'},
         method: 'POST',
         body: JSON.stringify({"mode": mode})
     })
     .then(function(response){
+        // gets back db data for mode in list with scores and users
         return response.json();
     })
     .then(data => {
+        // updates leaderboard table with data 
         console.log(data);
         let users = data.pop();
         console.log(users);
@@ -34,8 +39,8 @@ let py_data = (mode) => {
     })
 }
 
+// when dropdown is changed/new mode is selected, calls fxn to get data
 let dropdown = document.getElementById("modes")
-
 let getMode = () => {
     mode = dropdown.value
     console.log(mode)
@@ -43,5 +48,5 @@ let getMode = () => {
 }
 dropdown.addEventListener('click', getMode)
 
-let table = document.getElementById("table")
-console.log(table)
+// when page is first loaded, starts off with basic mode data 
+py_data('basic')
